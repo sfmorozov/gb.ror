e9755a531c69754dd8b08f560abf55ce3154162c
Sergey Morozov <sfmorozov@gmail.com>
2021-12-11 15:01:54 +0500

    require -> to Gemfile

--- lesson1.1/get_phone.rb
+++ lesson1.1/get_phone.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require 'rubygems'
-require 'nokogiri'
-require 'open-uri'
+require 'bundler/setup'
+
+Bundler.require(:default)
 
 src = Nokogiri::HTML(URI.open('https://gb.ru')).css('a')
 puts src.find { |x| x.text.match(/^\+|[0-9]|\d/) }.text
